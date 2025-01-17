c231adfeef4ff00702f7572f7a4bfde6cecf7c08
Liren Tu <tuliren@gmail.com>
2025-01-16 17:13:59 -0800

    Add parameter liquid server

--- ruler.sh
+++ ruler.sh
@@ -3,27 +3,41 @@
 set -euo pipefail
 
 usage() {
-  echo "Usage: $0 --liquid-api-key <LIQUID_API_KEY>"
+  echo "Usage: $0 --liquid-api-key <LIQUID_API_KEY> [--liquid-server <LIQUID_SERVER>]"
   exit 1
 }
 
-if [ "$1" != "--liquid-api-key" ] || [ -z "$2" ]; then
-  echo "Error: missing Liquid API key."
-  usage
-fi
+# TODO: change to inference-1
+LIQUID_SERVER="inference-dev.liquid.ai"
 
-LIQUID_API_KEY=$2
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --liquid-api-key)
+      LIQUID_API_KEY="$2"
+      shift 2
+      ;;
+    --liquid-server)
+      LIQUID_SERVER="$2"
+      shift 2
+      ;;
+    *)
+      echo "Error: Unknown parameter $1"
+      usage
+      ;;
+  esac
+done
 
 if [ -z "$LIQUID_API_KEY" ]; then
-  echo "Error: Liquid API key cannot be empty."
-  show_usage
+  echo "Error: missing Liquid API key."
+  usage
 fi
 
 # This script works when starting in a fresh conda environment with python=3.11
 # conda create -n ruler python=3.11
 # conda activate ruler
 cd RULER
-bash install_script.sh
+# bash install_script.sh
 
 export OPENAI_API_KEY="$LIQUID_API_KEY"
+export LIQUID_SERVER="$LIQUID_SERVER"
 bash run_ruler.sh

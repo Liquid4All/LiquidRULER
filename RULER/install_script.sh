22105b99e83b07d536c0c83595d5f515ccd883e7
Liren Tu <tuliren@gmail.com>
2025-02-12 22:29:02 -0800

    Download nltk in local script

--- RULER/install_script.sh
+++ RULER/install_script.sh
@@ -11,3 +11,4 @@ python -m pip install torchaudio --upgrade
 cd scripts/data/synthetic/json/
 python download_paulgraham_essay.py
 bash download_qa_dataset.sh
+python -c "import nltk; nltk.download('punkt'); nltk.download('punkt_tab')"
